c2661e4b9b959a3981fa68468635dc72146583f9
Janani235 <jananivairam235@gmail.com>
2025-03-07 12:18:41 +0530

    Add files via upload

---
+++ position.java
@@ -0,0 +1,24 @@
+package day1;
+import java.util.*;
+public class position {
+		public static void main(String[]args) {
+			Scanner s=new Scanner(System.in);
+			String str =s.nextLine();
+			int position = 0;
+			int length =str.length();
+			if (length % 2 == 0) 
+			{
+				position = length/2-1;
+				length = 2;
+				System.out.println(str.substring(position,position+length));
+			}
+			else 
+			{
+				position = length / 2;
+				System.out.println(str.charAt(position));
+			}
+		}
+
+	}
+
+
