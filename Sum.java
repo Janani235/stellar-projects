fe1b9ce67f2c642798506fd35c18f045143d66e7
Janani235 <jananivairam235@gmail.com>
2025-03-07 12:34:06 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,32 @@
+package day1;
+import java.util.Scanner;
+public class Sum {
+	public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int sum=0;
+	int arr[][]=new int [n][n];
+	
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++) {
+			arr[i][j]=sc.nextInt();
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++) {
+			 System.out.print(arr[i][j]);
+		}
+		 System.out.println();
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++) {
+			if((i==j)||(i>j)){
+				sum=sum+arr[i][j];
+			}
+		}
+	}
+	System.out.println("sum"+sum);
+}
+}
+
+	
