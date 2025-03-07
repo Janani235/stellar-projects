387edc4e878a512d1e706529f3bd10eab1037da6
Janani235 <jananivairam235@gmail.com>
2025-03-07 12:03:35 +0530

    Add files via upload

---
+++ luckywinner.java
@@ -0,0 +1,20 @@
+
+package jananiv;
+
+import java.util.*;
+public class lucwin{
+public static void main(String[]args) 
+{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+        if(a%10==3||a%10==8) {
+	       System.out.println("she is luck winer");
+        }
+ else {
+	       System.out.println("she is not a luck winer");
+      }
+  }
+
+}
+
+
