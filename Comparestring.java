3a416cb6ce6ecfe9ec307c62cdfa2952573f6801
Janani235 <jananivairam235@gmail.com>
2025-03-07 12:43:44 +0530

    Add files via upload

---
+++ Comparestring.java
@@ -0,0 +1,13 @@
+package day1;
+import java.util.*;
+public class Comparestring {
+		public static void main(String[]args) {
+			Scanner sc=new Scanner(System.in);
+			String s1=sc.nextLine();
+			String s2=sc.nextLine();
+			System.out.println(s1.compareTo(s2));
+		}
+
+}
+
+
